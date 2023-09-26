db7187ce9394bd28ec954b36e57b3c52d8c2d268
Iryna Hryshanovich <iryna.hryshanovich@4cinsights.com>
2023-09-26 14:12:51 -0700

    added all microblog files

---
+++ app.py
@@ -0,0 +1,37 @@
+import datetime
+import os
+import certifi
+
+from flask import Flask, render_template, request
+from pymongo import MongoClient
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
+# app factory help to create only one app
+def create_app():
+    app = Flask(__name__)
+    client = MongoClient(os.getenv("MONGODB_URI"), tlsCAFile=certifi.where())
+
+    app.db = client.microblog
+
+    @app.route("/", methods=["GET", "POST"])
+    def home():
+        if request.method == "POST":
+            # form is dictionary
+            entry_content = request.form.get("content")
+            formatted_date = datetime.datetime.today().strftime("%Y-%m-%d")
+            app.db.entries.insert_one({"content": entry_content, "date": formatted_date})
+
+        entries_with_date = [
+            (
+                entry["content"],
+                entry["date"],
+                datetime.datetime.strptime(entry["date"], "%Y-%m-%d").strftime("%b %d")
+            )
+            for entry in app.db.entries.find({})
+        ]
+        return render_template("home.html", entries=entries_with_date)
+
+    return app
